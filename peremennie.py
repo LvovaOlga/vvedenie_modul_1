89628cbcbaa02a4c2833342642671e4f47e06e8b
LvovaOlga <olyalions@gmail.com>
2024-10-09 11:41:47 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,5 @@
+hw_done="всего задач:12,"
+hours_spent="затрачено часов:1,5,"
+name="Python,"
+time_for_a_task="среднее время выполнения:"
+print(name,hw_done,hours_spent,time_for_a_task,float(1.5/12))
