03d4a24acd9b4bb785769a91fbba648fad7b0c17
LvovaOlga <olyalions@gmail.com>
2024-10-18 10:56:32 +0300

    Update peremennie.py
    Исправлены ошибки

--- peremennie.py
+++ peremennie.py
@@ -1,5 +1,5 @@
-hw_done="всего задач:12,"
-hours_spent="затрачено часов:1,5,"
-name="Python,"
-time_for_a_task="среднее время выполнения:"
-print(name,hw_done,hours_spent,time_for_a_task,float(1.5/12))
+hw_done=12
+hours_spent=1.5
+name='Python'
+time_for_a_task= hours_spent/hw_done
+print(name,hw_done,hours_spent,time_for_a_task)
