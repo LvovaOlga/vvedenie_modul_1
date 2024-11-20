75ae549ee9a5f4cff9c422a79b90112b621284b5
LvovaOlga <olyalions@gmail.com>
2024-11-20 10:20:00 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,22 +1,16 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
     a = '@'
-    while 1 > 0:
-        if a not in recipient and sender:
+    if a in recipient and sender:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    else:
+        if not recipient.endswith(('.com', '.ru', '.net')) or not sender.endswith(('.com', '.ru', '.net')):
             print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-            break
-        else:
-            if not recipient.endswith(('.com', '.ru', '.net')) or not sender.endswith(('.com', '.ru', '.net')):
-                print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-            break
-        if sender == recipient:
-            print('Нельзя отправить письмо самому себе!')
-            break
-        elif sender == 'university.help@gmail.com':
-            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-            break
-        elif sender != 'university.help@gmail.com':
-            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-            break
+    if sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
@@ -26,4 +20,3 @@ send_email('Вы видите это сообщение как лучший ст
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
